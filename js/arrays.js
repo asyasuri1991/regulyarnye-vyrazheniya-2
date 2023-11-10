f50f42f0abe6c0ebe37b2998402534064cdf575c
asyasuri1991 <asyasuri@gmail.com>
2023-11-10 11:19:27 +0300

    задача2 решена

--- js/arrays.js
+++ js/arrays.js
@@ -6,9 +6,24 @@ function task1()
     let adress=url.test(str);
     return adress;
     }
-    let str=prompt("Введите URL адрес");
+    let str=prompt("Введите URL");
     if (isURL(str))
         alert("Правильно");
     else
         alert("Некорректный ввод");
+}
+
+function task2()
+{
+    function isName(str)
+    {
+    let re = /^[a-zA-Z$_]{1}[\w$]*/;
+    let flag=re.test(str);
+    return flag;
+    }
+    let str=prompt("Введите имя переменной");
+    if (isName(str))
+        alert("Имя переменной введено правильно");
+    else
+        alert("Имя переменной введено неправильно");
 }
